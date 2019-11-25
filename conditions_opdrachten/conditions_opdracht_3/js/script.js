431bf5ba2e506245853b5c709579b9854b70cdb1
unknown <you@example.com>
2019-11-25 14:11:31 +0100

    opdracht 3

--- conditions_opdrachten/conditions_opdracht_3/js/script.js
+++ conditions_opdrachten/conditions_opdracht_3/js/script.js
@@ -1,1 +1,15 @@
 //javascript code
+function myFunction() {
+    var txt = prompt("Hoeveel geld heb je");
+    if (txt == 500) {
+    var txt = alert("je hebt salaris gekregen")
+    }
+  else if (txt < 500){
+    var txt = alert("je hebt een bonus gekregen")
+  }
+
+  else{
+      var txt = alert("niks")
+  }
+
+}
