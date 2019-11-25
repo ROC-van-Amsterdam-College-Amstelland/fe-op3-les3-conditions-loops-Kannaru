2f8f115401cf75178e437872848789c80e01425c
unknown <you@example.com>
2019-11-25 14:58:45 +0100

    opdracht 4

--- conditions_opdrachten/conditions_opdracht_4/js/script.js
+++ conditions_opdrachten/conditions_opdracht_4/js/script.js
@@ -1,1 +1,6 @@
-//javascript code
+//javascript codefunction myFunction() {
+function waarde()
+{
+var myNumber = 10 ;
+document.getElementById("plek").innerHTML = "De variabele heeft de waarde:" + myNumber ;
+}
