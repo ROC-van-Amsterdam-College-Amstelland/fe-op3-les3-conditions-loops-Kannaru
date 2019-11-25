e5a5c1177f86d7e7e062eca8d072952e65320b3a
unknown <you@example.com>
2019-11-25 13:22:01 +0100

    opdracht 1

--- conditions_opdrachten/conditions_opdracht_1/js/script.js
+++ conditions_opdrachten/conditions_opdracht_1/js/script.js
@@ -1,1 +1,11 @@
 //javascript code
+function myFunction() {
+    var txt = prompt("Hoeveel geld heb je");
+    if (txt >= 25) {
+    var txt = alert("mooi")
+    }
+  else{
+    var txt = alert("Sorry, je hebt te weinig geld")
+  }
+
+}
