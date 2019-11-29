95ce6b8aab8b0620b3b5ad673ffe516fb727a06d
unknown <you@example.com>
2019-11-29 11:00:56 +0100

    loops1

--- loops_opdrachten/loops_opdracht_1/js/script.js
+++ loops_opdrachten/loops_opdracht_1/js/script.js
@@ -1,1 +1,10 @@
 //javascript code
+function myFuntion(){
+    var text = "";
+    var i;
+    for (i = 0; i < 6; i++) {
+      text += "Nummer " + i + "<br>";
+      document.getElementById("test").innerHTML = text;
+    }
+    document.getElementById("test").innerHTML = text;
+    }
